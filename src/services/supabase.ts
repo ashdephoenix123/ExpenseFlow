caf23935600df95377e619857307cd0e68240728
ashdephoenix123 <akashsarki12345@gmail.com>
2026-03-23 07:59:17 +0530

    added async storage for persistant store

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,10 +1,12 @@
 import 'react-native-url-polyfill/auto';
 import 'react-native-get-random-values';
 import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
+    storage: AsyncStorage,
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: false,
